645875e8da37587620e3a736679599352f7a70aa
Alexander Lazovski <Lazovski1991@gmail.com>
2020-09-28 18:54:52 +0300

    login form

---
+++ src/main/java/com/example/demo/rest/AuthController.java
@@ -0,0 +1,21 @@
+package com.example.demo.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/success")
+    public String getSuccess() {
+        return "success";
+    }
+
+}
