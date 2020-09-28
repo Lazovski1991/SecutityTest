2cb9e23d9d1befa6a1019e0c93422dbcdfd0da35
Alexander Lazovski <Lazovski1991@gmail.com>
2020-09-28 17:49:41 +0300

    basic autorities

---
+++ src/main/java/com/example/demo/model/Role.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum Role {
+    USER(Set.of(Permission.DEVELOPERS_READ)),
+    ADMIN(Set.of(Permission.DEVELOPERS_READ, Permission.DEVELOPERS_WRITE));
+
+    private final Set<Permission> setPermissions;
+
+
+    Role(Set<Permission> setPermissions) {
+        this.setPermissions = setPermissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return setPermissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
+}
---
+++ src/main/java/com/example/demo/rest/DeveloperRestController.java
@@ -0,0 +1,44 @@
+package com.example.demo.rest;
+
+import com.example.demo.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestController {
+
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Alexander", "Sergeevich"),
+            new Developer(3L, "Sergei", "Petrov")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream().filter(dev -> dev.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('developers:write)")
+    public Developer create(@RequestBody Developer developer) {
+        DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping
+    @PreAuthorize("hasAuthority('developers:write)")
+    public void deleteById(@PathVariable Long id) {
+        this.DEVELOPERS.removeIf(dev -> dev.getId().equals(id));
+    }
+}
